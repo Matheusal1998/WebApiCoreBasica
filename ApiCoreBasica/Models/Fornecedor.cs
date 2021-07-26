98f427572979f7250dcd71c012a815280a596e0c
Matheus Lima <matheus.al1998@gmail.com>
2021-07-26 14:19:13 -0300

    [FEATURE] Projeto básico web api com .net core

---
+++ ApiCoreBasica/Models/Fornecedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCoreBasica.Models
+{
+    public class Fornecedor
+    {
+        public Guid Id { get; set; }  
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public int TipoFornecedor { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
